0e4cf97ed5fe2d5b335afb7a2284c4bb32dee192
Ellie Agulnek <elliebrie@gmail.com>
2025-09-18 17:33:45 +0200

    Player Movement Script

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,15 +2,76 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Rigidbody2D playerRB;
+    [SerializeField] float speed;
+    [SerializeField] int maxSpeed;
+    [SerializeField] float acceleration;
+    [SerializeField] float friction;
+    [SerializeField] KeyCode leftKey;
+    [SerializeField] KeyCode rightKey;
+    [SerializeField] KeyCode upKey;
+    [SerializeField] KeyCode downKey;
+
+
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        playerRB.linearVelocity = new Vector2(0, 0);
+
+        if (Input.GetKey(leftKey))
+        {
+            moveX(-speed);
+        }
+
+        if (Input.GetKey(rightKey))
+        {
+            moveX(speed);
+        }
+        if (Input.GetKey(upKey))
+        {
+            moveY(speed);
+        }
+        if (Input.GetKey(downKey))
+        {
+            moveY(-speed);
+        }
     }
+
+    void moveX(float velocity)
+    {
+        playerRB.linearVelocityX += velocity;
+    }
+
+    void moveY(float velocity)
+    {
+        playerRB.linearVelocityY += velocity; 
+    }
+
+    public KeyCode getLeftKey()
+    {
+        return leftKey;
+    }
+
+    public KeyCode getRightKey()
+    {
+        return rightKey;
+    }
+
+    public KeyCode getUpKey()
+    {
+        return upKey;
+    }
+
+    public KeyCode getDownKey()
+    {
+        return downKey;
+    }
+
 }
+
+
+
