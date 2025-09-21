2f88b2e0624340b26f93c9399a35c237185d8e4e
Maira Molina <mpmolina1320@gmail.com>
2025-09-21 14:05:03 +0200

    Able to pickup in all direc.

--- Assets/Scripts/Player/PickUpController.cs
+++ Assets/Scripts/Player/PickUpController.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public class PickUpController : MonoBehaviour
 {
 
-    public Transform holdSpot;
-    public LayerMask pickupMask;
+    [SerializeField] private float checkRange = 1f;
+
+    [SerializeField] public Transform holdSpot;
+    [SerializeField] public LayerMask pickupMask;
     public Vector3 Direction{get; set;}
     public float radius = 0.4f;
 
@@ -19,24 +21,42 @@ public class PickUpController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Direction = Vector2.right;
-        if(Input.GetKeyDown(KeyCode.LeftShift))
+        if(Input.GetKeyDown(KeyCode.LeftShift)){
+            checkAllDirections();
+        }
+        
+    }
+
+    private void checkAllDirections()
+    {
+        Debug.Log("CheckingAllDirections");
+        RaycastHit2D up = Physics2D.Raycast(transform.position, Vector2.up, checkRange, pickupMask);
+        RaycastHit2D down = Physics2D.Raycast(transform.position, Vector2.down, checkRange, pickupMask);
+        RaycastHit2D right = Physics2D.Raycast(transform.position, Vector2.right, checkRange, pickupMask);
+        RaycastHit2D left = Physics2D.Raycast(transform.position, Vector2.left, checkRange, pickupMask);
+
+        RaycastHit2D[] hits = new RaycastHit2D[] {up, down, right, left};
+        foreach (RaycastHit2D hit in hits){
+            pickUpItem(hit);
+        }
+    }
+
+    private void pickUpItem(RaycastHit2D hit)
+    {
+        if (hit)
         {
+            Debug.Log("EvaluatingHit");
             if(itemHolding){
 
             }else
             {
-                Collider2D pickUpItem = Physics2D.OverlapCircle(transform.position + Direction, radius, pickupMask);
-                if (pickUpItem)
-                {
-                    itemHolding = pickUpItem.gameObject;
-                    itemHolding.transform.position = holdSpot.position;
-                    itemHolding.transform.parent = transform;
-                    if(itemHolding.GetComponent<Rigidbody2D>()){
-                        itemHolding.GetComponent<Rigidbody2D>().simulated = false;
-                    }
-
-
+                //Collider2D pickUpItem = Physics2D.OverlapCircle(hit.point, radius, pickupMask);
+  
+                itemHolding = hit.collider.gameObject;
+                itemHolding.transform.position = holdSpot.position;
+                itemHolding.transform.parent = transform;
+                if(itemHolding.GetComponent<Rigidbody2D>()){
+                    itemHolding.GetComponent<Rigidbody2D>().simulated = false;
                 }
             }
         }
